225a4cef561f93a96a80bcd55710faf0830568b5
John Deen Bucog <JDBucog@yahoo.com>
2022-11-15 00:07:02 -0800

    add custom document to change html lang to en, add Header and Layout components to show on multiple pages, add styling for Header and layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
 import styles from "../styles/Layout.module.css"
 import Navigator from "./Navigator"
+import Header from "./Header"
 const Layout = ({ children }) => {
   return (
     <>
       <Navigator />
+
       <div className={styles.container}>
+        <Header />
         <main className={styles.main}>{children}</main>
       </div>
     </>
